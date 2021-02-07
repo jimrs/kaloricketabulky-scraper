6156e1541b24ebce770b3ec5b53b4952d495e8c7
Jiri Mares <marj39@vse.cz>
2021-02-07 11:55:12 +0100

    added regex for parsing out food names

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import re
 
 def getPages(start=1, limit=20026):
     # posledni validni je page=20025
@@ -10,5 +11,14 @@ def getPages(start=1, limit=20026):
         with open('raw/tab' + str(i), 'w') as f:
             f.write(data.text)
 
+def parseNames(raw):
+    with open(raw, 'r') as f:
+        data = f.read()
+        match = re.findall('(?<=reload>).+?(?=</a>)', data)
+        match = match[0:10]
+        print(match)
+        print(len(match))
+
 if __name__ == '__main__':
-    getPages(2164, 10001)
+    # getPages(2164, 10001)
+    parseNames('raw/tab1')
