185774fe7b253782efc88c0f78b1905885e4f73b
Jiri Mares <marj39@vse.cz>
2021-02-11 22:17:16 +0100

    refactoring. outputting to csv file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import re
 import unicodedata
+import csv
 
 def getPages(start=1, limit=20026):
     # posledni validni je page=20025
@@ -15,26 +16,62 @@ def getPages(start=1, limit=20026):
 def parseData(raw):
     with open(raw, 'r') as f:
         data = f.read()
-        names = re.findall('(?<=reload>).+?(?=</a>)', data)
-        names = names[0:10]
-        print(names)
+        names = _parseNames(data)
+        numbers = _parseNumbers(data)
 
-        numbers = re.findall('(?<=td md-cell hide-xs>)\s\d*?\s?,?\d*?(?=\s?</td>)', data)
+    # TODO cyklus pro projeti vsech souboru ve slozce raw
+    # TODO do promenne table pridavat dalsi a dalsi hodnoty
+    # TODO nasledne zapsat
+    table = _createTable(names, numbers)
+    _writeTable(table)
 
-        numbers_norm = []
-        for item in numbers:
-            item = item.strip()
-            item_norm = item.replace(u'\xa0', u' ')
-            numbers_norm.append(item_norm)
 
-        print(numbers_norm)
-        # can be used for testing, must always equal 50
-        print(len(numbers_norm)
+def _writeTable(table):
+    with open('out/test.csv', 'w', newline='') as f:
+        wr = csv.writer(f, quoting=csv.QUOTE_ALL)
+        header = ['Název', 'Energetická hodnota (kcal)', 'Bílkoviny (g)', 'Sacharidy (g)', 'Tuky (g)', 'Vláknina (g)']
+        wr.writerow(header)
+        wr.writerows(table)
+
+
+def _createTable(names, numbers):
+    table = []
+    for index, item in enumerate(names):
+        row = [item]
+        for i in range (index * 5, (index * 5) + 5):
+            row.append(numbers[i])
+        table.append(row)
+
+    # print(table)
+    # len must always be 10
+    # print(len(table))
+    return table
+
+
+def _parseNames(raw_data):
+    names = re.findall('(?<=reload>).+?(?=</a>)', raw_data)
+    names = names[0:10]
+    #print(names)
+    return names
+
+
+def _parseNumbers(raw_data):
+    numbers = re.findall('(?<=td md-cell hide-xs>)\s\d*?\s?,?\d*?(?=\s?</td>)', raw_data)
+
+    numbers_norm = []
+    for item in numbers:
+        item = item.strip()
+        item_norm = item.replace(u'\xa0', u' ')
+        numbers_norm.append(item_norm)
+
+    #print(numbers_norm)
+    # can be used for testing, must always equal 50
+    #print(len(numbers_norm))
+    return numbers_norm
+
 
 if __name__ == '__main__':
     # getPages(2164, 10001)
     parseData('raw/tab1')
 
-    #TODO add names of columns
-    #TODO combine together
-    #TODO export to csv table/json
+    #TODO json
